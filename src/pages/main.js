05c6174779f8e6f9951ab19542e87618ee0204df
LouisXVBTW <41397906+LouisXVBTW@users.noreply.github.com>
2021-03-27 13:28:10 +0000

    update node_modules

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,49 +8,34 @@ function Main() {
     <Fragment>
       <Navb/>
       <Container fluid>
-      <br/>
-    
-    <Col>
-      <Row>
-      <Card className="text-center">
-      <Card.Header>Idk some placeholder</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-      </Row>
-      <Row>
-      <Card className="text-center">
-      <Card.Header>Idk some placeholder</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-      </Row>
-      
-    
-    <Col>
-    <Card className="text-center">
-      <Card.Header>Idk some placeholder</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Col>
-    </Col>
-    </Container>
+        <br/>
+        <Row>
+          <Col>
+              <Card className="text-center">
+                <Card.Header>Idk some placeholder</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+              <Card className="text-center">
+                <Card.Header>Idk some placeholder</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+          </Col>
+          <Col><h2>THISISISISSI</h2></Col>
+        </Row>
+        
+      </Container>
     </Fragment>
     
   )
